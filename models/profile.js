2d5ce30e552f2ab14acefa96ff5b334e9645be45
Shivam Akhouri <shivamakhouri1142@gmail.com>
2021-11-03 14:52:33 +0530

    Added Image upload facility in create question menu

--- models/profile.js
+++ models/profile.js
@@ -1,9 +1,10 @@
 export default class Profile{
-    constructor(name, level, skillBadge, badge, focustime) {
+    constructor(name, level, skillBadge, badge, focustime, groups) {
         this.name = name;
         this.level  = level;
         this.skillBadge = skillBadge;
         this.badge = badge;
         this.focustime = focustime;
+        this.groups = groups;
     }
 }
--- screens/createQuestion_screen.jsx
+++ screens/createQuestion_screen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Image } from 'react-native';
 import {
     widthPercentageToDP as wp,
     heightPercentageToDP as hp,
@@ -8,15 +8,21 @@ import {Input, Button} from 'react-native-elements';
 import LinearGradient from 'react-native-linear-gradient';
 import Question from '../models/question';
 import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
 
 export default function CreateQuestion({navigation}){
     const [question, setQeustion] = React.useState("");
     const [badge, setBadge] = React.useState('');
     const [photoUrl, setPhotoUrl] = React.useState("");
     function handleClick(){
+        var storageRef = storage().ref('dummyPhoto.jpg');
         var q = new Question(question, "", badge);
         console.log(q.printQuestion());
         console.log(photoUrl);
+        if(photoUrl!= ""){
+            const upload = storageRef.putFile("file://"+photoUrl);
+            upload.then(()=>console.log("Image uploaded"));
+        }
         // firestore()
         //     .collection('questions')
         //     .add({
@@ -26,6 +32,7 @@ export default function CreateQuestion({navigation}){
         //     .then(() => {
         //         console.log('User added!');
         //     });
+        console.log("Done Dana Dan");
     }
     return (
         <>
@@ -38,6 +45,9 @@ export default function CreateQuestion({navigation}){
                             <Input placeholder="Enter the question" numberOfLines={3} value={question} onChangeText={(val)=>setQeustion(val)}/>
                             <Input placeholder="Enter badge" value={badge} onChangeText={(val)=>setBadge(val)}/>
                         </View>
+                        {photoUrl !="" &&
+                        <Image source={{uri:"file://"+photoUrl, width: wp(90), height: hp(30)}} />
+                        }   
                         <View style={{position: 'absolute', width: wp(90), bottom: hp(10)}}>
                             <Button title="Take Picture" style={{marginBottom: 10}} buttonStyle={{backgroundColor: 'green'}}
                             onPress={()=>navigation.navigate('Camera', {setPhotoUri: setPhotoUrl})}/>
