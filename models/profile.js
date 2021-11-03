dec0c9b1694857995f95ca3d717535c2e34feb89
Shivam Akhouri <shivamakhouri1142@gmail.com>
2021-11-04 00:40:15 +0530

    Configured firestore to add User data on new SignUp

--- models/profile.js
+++ models/profile.js
@@ -1,6 +1,10 @@
 export default class Profile{
-    constructor(name, level, status, skillBadge, badge, focustime, groups, totalTime, averageTime) {
+    constructor(uid, name, email, photoUrl, phoneNumber,level, status, skillBadge, badge, focustime, groups, totalTime, averageTime) {
+        this.uid = uid;
         this.name = name;
+        this.email = email;
+        this.photoUrl = photoUrl;
+        this.phoneNumebr = phoneNumber;
         this.level  = level;
         this.status = status;
         this.skillBadge = skillBadge;
--- screens/login_screen.jsx
+++ screens/login_screen.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Button } from 'react-native';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 GoogleSignin.configure({
-  webClientId: '686843872188-na0qlld5etg800c9em2keg6glotlpefp.apps.googleusercontent.com',
+  webClientId: '686843872188-pttd6f6idrv9ebnhplvv7tei30pti43e.apps.googleusercontent.com',
 });
 
 async function onGoogleButtonPress() {
@@ -12,16 +14,59 @@ async function onGoogleButtonPress() {
   
     // Create a Google credential with the token
     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-  
     // Sign-in the user with the credential
     return auth().signInWithCredential(googleCredential);
+
   }
 
 export default function GoogleSignIn() {
   return (
+      <>
     <Button
       title="Google Sign-In"
-      onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+      onPress={() => {
+          onGoogleButtonPress().then(() => {
+            console.log('Signed in with Google!');
+            console.log("Adding user data");
+          
+            //Adding user data to the user collection
+
+            //Checking for pre existence
+            let userData = auth().currentUser;
+            firestore().collection('Users').doc(userData.uid).get().then(res=>{
+                if(!res.exists){
+                    firestore()
+                .collection('Users')
+                .doc(userData.uid)
+                .set({
+                    uid: userData.uid,
+                    name: userData.displayName,
+                    email: userData.email,
+                    phoneNumber: userData.phoneNumber,
+                    photoUrl: userData.photoURL,
+                    level: "0",
+                    status: "Online",
+                    skillBadge: "Uchi Deshi",
+                    focusTime: 0,
+                    groups: [],
+                    totalTime: 0,
+                    averageTime: 0,
+                })
+                .then(() => {
+                    console.log('User added!');
+                });
+                }
+            })
+            });
+        }}
     />
+    <Button
+      title="sign OUt"
+      onPress={()=>{auth()
+        .signOut()
+        .then(() => console.log('User signed out!'));}
+      }
+    />
+    </>
   );
 }
