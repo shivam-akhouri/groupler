4b3d874d9685a107b09a79458c6d1cff6b3396d2
Shivam Akhouri <shivamakhouri1142@gmail.com>
2021-11-06 15:35:15 +0530

    Answer Tile made dynamic

--- components/answertile.jsx
+++ components/answertile.jsx
@@ -5,17 +5,12 @@ import {
     heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 
-export default function AnswerTile() {
+export default function AnswerTile({answer}) {
     return (
         <>
             <View style={styles.container}>
                 <Text style={styles.text}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                      nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-                       eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
-                       in culpa qui officia deserunt mollit anim id est laborum.
+                    {answer}
                 </Text>
                 <View style={styles.line}></View>
             </View>
