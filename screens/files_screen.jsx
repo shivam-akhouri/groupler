d8a26a4a3a712711a99ead495b6085365729f4e1
Shivam Akhouri <shivamakhouri1142@gmail.com>
2021-11-10 09:16:28 +0530

    Files page added

--- screens/files_screen.jsx
+++ screens/files_screen.jsx
@@ -10,10 +10,20 @@ import LottieView from 'lottie-react-native';
 export default function FileScreen(){
     return (
         <View style={styles.container}>
-            <View>
-                <View>
-                    <LottieView source={require('../assets/file.json')}
+            <View style={styles.listFile} >
+                <View style={styles.tile}>
+                    <View style={styles.innertile}>
+                        <LottieView source={require('../assets/file.json')} autoPlay loop={false} style={{height: hp(14), width: wp(20)}}/>
+                        <View style={styles.texttile}>
+                            <Text style={styles.text} numberOfLines={2}>Hello</Text>
+                        </View>
+                    </View>
                 </View>
+            </View>
+            <View style = {styles.footer}> 
+                <TouchableOpacity>
+                    <LottieView source={require('../assets/addfile.json')} autoPlay loop={false} style={{height: hp(12)}}/>
+                </TouchableOpacity>
             </View>
         </View>
     );
@@ -24,5 +34,43 @@ const styles = StyleSheet.create({
         width: wp(100),
         height: hp(100),
         backgroundColor: 'black',
+        alignItems: 'center'
+    },
+    text:{
+        color: 'white',
+        fontSize: wp(6.),
+    },
+    innertile:{
+        backgroundColor: '#303030',
+        alignItems: 'center',
+        padding: hp(0.5),
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#5822b5',
+        margin: hp(0.5)
+    },
+    tile:{
+        borderRadius: 13,
+        borderWidth: 2,
+        borderColor: '#b52222',
+        width: wp(40),
+        height: hp(25.5)
+    },
+    footer:{
+        width: wp(100),
+        backgroundColor: '#454545',
+        alignItems: 'center',
+        height: hp(12),
+        borderTopLeftRadius: wp(100),
+        borderTopRightRadius: wp(100),
+    },
+    listFile:{
+        width: wp(100),
+        height: hp(78),
+    },
+    texttile:{
+        height: hp(8.3),
+        justifyContent: 'center'
     }
+
 })
